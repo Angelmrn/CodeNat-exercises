2abda98fc9618fd869654a5ba3b0938681f82dd3
Angel Moran <122565686+Angelmrn@users.noreply.github.com>
2025-07-07 19:11:35 -0600

    second commit

---
+++ 159exercises/ej60.js
@@ -0,0 +1,8 @@
+function removeFandL(str){
+    return str.substring(1, str.length - 1);// Remove the first and last characters
+}
+
+console.log(removeFandL("Python"));      
+console.log(removeFandL("JavaScript"));  
+console.log(removeFandL("PHP"));  
+console.log(removeFandL("JS"));         
