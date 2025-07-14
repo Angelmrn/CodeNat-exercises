0a81dc22a3aed81925608144091c59c59df402eb
Angel Moran <122565686+Angelmrn@users.noreply.github.com>
2025-07-13 19:09:45 -0600

    muiButton: Add MUI Button component for Week 2 challenge

---
+++ week2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MuiButtonExample from './muiButton';
+
+function App() {
+  return (
+    <div>
+      <h1>Week 2 Challenge</h1>
+      <MuiButtonExample />
+    </div>
+  );
+}
+
+export default App;
